3be609a23a4b94c47002957d311bd2c6e8abc682
Langton <langtonfavor@gmail.com>
2022-09-08 16:12:17 +0200

    coffee shop app

---
+++ src/app/logic/coffee.ts
@@ -0,0 +1,20 @@
+import { TastingRating } from "./TastingRating";
+import { PlaceLocation } from "./PlaceLocation";
+
+
+export class Coffee
+{
+    //properties
+    _id:string;
+    type:string;
+    rating:number;
+    notes:string;
+    tastingRating:TastingRating;
+
+    constructor(public name: string="",
+     public place: string="",
+      public location: PlaceLocation=null) {
+          this.location=new PlaceLocation();
+          this.tastingRating=new TastingRating();
+    }
+    }
